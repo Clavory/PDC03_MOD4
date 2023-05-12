7c1ab149253bca9080b8a2cb904fc093d52d98cb
Clavory <allapitan.nathaniel@auf.edu.ph>
2023-05-12 10:15:59 +0800

    second commit

---
+++ PDC03_MODULE4/PDC03_MODULE4/activity05.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity05 : ContentPage
+    {
+        List<Contacts> contacts;
+        public activity05()
+        {
+            InitializeComponent();
+            SetupData();
+            listView.ItemsSource = contacts;
+        }
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        void SetupData()
+        {
+            contacts = new List<Contacts>();
+            contacts.Add(new Contacts
+            {
+                Name = "Nathaniel Allapitan",
+                Age = 21,
+                Occupation = "Student",
+                Country = "Philippines"
+
+            });
+
+            contacts.Add(new Contacts
+            {
+                Name = "Sync Allapitan",
+                Age = 22,
+                Occupation = "Student",
+                Country = "Philippines"
+
+            });
+
+            contacts.Add(new Contacts
+            {
+                Name = "Clavory Allapitan",
+                Age = 23,
+                Occupation = "Student",
+                Country = "Philippines"
+
+            });
+
+            contacts.Add(new Contacts
+            {
+                Name = "Niel Allapitan",
+                Age = 19,
+                Occupation = "Student",
+                Country = "Philippines"
+
+            });
+
+            contacts.Add(new Contacts
+            {
+                Name = "Nathaniel Allapitan",
+                Age = 25,
+                Occupation = "Testes",
+                Country = "Philippines"
+
+            });
+        }
+    }
+}
